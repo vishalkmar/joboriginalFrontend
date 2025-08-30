a86278011fdc95c61d3c6f2e1be2091526699188
vishalkmar <vk722413@gmail.com>
2025-08-30 20:51:18 +0530

    fixed frontend url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,3 +2,4 @@ export const USER_API_END_POINT="https://joboriginalbackend.onrender.com/api/v1/
 export const JOB_API_END_POINT="https://joboriginalbackend.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://joboriginalbackend.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://joboriginalbackend.onrender.com/api/v1/company";
+
